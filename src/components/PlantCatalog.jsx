59c9758812d88479863b147a873932cd5242dfef
Lucie Zhong <luciez@Lucies-MacBook-Air.local>
2025-10-08 17:10:53 -0700

    refactored components

--- src/components/PlantCatalog.jsx
+++ src/components/PlantCatalog.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Plant from "./Plant";
 import myPlants from "../data/myPlants.json";
 
 export default function PlantCatalog() {
   const [plants, setPlants] = useState(myPlants);
 
-  function addPlant(plant) {
-    setPlants([...plants, plant]);
-  }
-
   return (
     <div className="catalog">
       <h2>My Plants</h2>
       <div className="plants-container">
+        <button className="add-button">
+          <span>+</span>
+        </button>
         {plants.map((p) => (
           <Plant key={p.id} data={p} />
         ))}
